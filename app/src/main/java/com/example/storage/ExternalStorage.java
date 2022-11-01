c5279147e2cdc9daab3351176c7ba5e9761dd26c
tur pahnlo <logan.hphan@gmail.com>
2022-11-01 21:45:12 +0700

    Thiết kế giao diện 3 activity

---
+++ app/src/main/java/com/example/storage/ExternalStorage.java
@@ -0,0 +1,14 @@
+package com.example.storage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExternalStorage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_external_storage);
+    }
+}
