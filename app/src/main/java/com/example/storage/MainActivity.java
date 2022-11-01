a35ce3134951640cf894e8b4970d4c52e2aa3928
tur pahnlo <logan.hphan@gmail.com>
2022-11-01 22:15:02 +0700

    thêm intent cho main activity

--- app/src/main/java/com/example/storage/MainActivity.java
+++ app/src/main/java/com/example/storage/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.storage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnInternal, btnExternal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnInternal = (Button) findViewById(R.id.btnInternal);
+        btnInternal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, InternalStorage.class); startActivity(intent);
+            }
+        });
+        btnExternal = (Button) findViewById(R.id.btnExternal);
+        btnExternal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, ExternalStorage.class); startActivity(intent);
+            }
+        });
     }
 }
