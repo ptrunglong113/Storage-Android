abdcc90df3c80d369c652367286e59ba623876ad
tur pahnlo <logan.hphan@gmail.com>
2022-11-08 20:49:50 +0700

    Ánh xạ Button

--- app/src/main/java/com/example/storage/MainActivity.java
+++ app/src/main/java/com/example/storage/MainActivity.java
@@ -6,13 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnTakePhoto, btnSave, btnDisplay;
+    ImageView imgViewTakePhoto;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTakePhoto = findViewById(R.id.btnTakePhoto);
+        btnSave = findViewById(R.id.btnSave);
+        btnDisplay = findViewById(R.id.btnDisplay);
+
     }
 }
