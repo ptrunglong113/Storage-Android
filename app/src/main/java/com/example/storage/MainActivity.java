7988ee013d71fc399f523a5b81da780c6298491a
Tạ Quang Khôi <khoi0941@gmail.com>
2022-11-15 22:43:46 +0700

    New changes from nvSon and ptLong

--- app/src/main/java/com/example/storage/MainActivity.java
+++ app/src/main/java/com/example/storage/MainActivity.java
@@ -1,23 +1,51 @@
 package com.example.storage;
 
-import android.content.Context;
-import android.content.ContextWrapper;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
-
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.SyncStateContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     Button btnTakePhoto, btnSave, btnDisplay;
     ImageView imgViewTakePhoto;
+
 
 
     @Override
@@ -26,60 +54,102 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnTakePhoto = findViewById(R.id.btnTakePhoto);
-        btnSave = findViewById(R.id.btnSave);
-        btnDisplay = findViewById(R.id.btnDisplay);
-        imgViewTakePhoto = findViewById(R.id.imgViewTakePhoto);
 
-        btnDisplay.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-            intent.setType("image/*");
-            startActivityForResult(intent, 1);
-        });
-    }
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+
+            btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pick_from_camera(v);
+                }
+            });
+
+            btnDisplay.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    pick_from_gallery(v);
+                }
+            });
+            btnSave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        saveToGallery();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+        }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if(requestCode == 1 && resultCode == RESULT_OK && data != null){
-            imgViewTakePhoto.setImageURI(data.getData());
-        } else if (requestCode == 2 && resultCode == RESULT_OK && data != null){
-            Bundle bundle = data.getExtras();
-            Bitmap bitmap = (Bitmap) bundle.get("data");
-            imgViewTakePhoto.setImageBitmap(bitmap);
+        switch (requestCode) {
+            case 0:
+                if (resultCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    imgViewTakePhoto.setImageBitmap(imageBitmap);
+                }
+                break;
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = data.getData();
+                    imgViewTakePhoto.setImageURI(selectedImage);
+                }
+                break;
         }
     }
-
-    private void LuuAnh() {
-
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(intent, 2);
-
-        ContextWrapper cw = new ContextWrapper(getApplicationContext());
-        // path to /data/data/yourapp/app_data/imageDir
-        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
-        // Create imageDir
-        File path = new File(directory,"profile.jpg");
+    public void pick_from_camera(View view) {
+        Intent takepicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takepicture,0);
     }
 
-    private String saveToInternalStorage(Bitmap bitmapImage, File path){
+    public void pick_from_gallery(View view) {
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto, 1);
+    }
 
+    private void saveToGallery() throws IOException {
+        Uri images;
+        ContentResolver contentResolver = getContentResolver();
 
-        FileOutputStream fos = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            images = MediaStore.Images.Media.getContentUri(MediaStore. VOLUME_EXTERNAL_PRIMARY) ;
+        }
+        else{
+            images = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        }
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, System.currentTimeMillis() + ".jpg");
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE,"image/*");
+        Uri uri = contentResolver.insert(images, contentValues);
+
         try {
-            fos = new FileOutputStream(mypath);
-            // Use the compress method on the BitMap object to write image to the OutputStream
-            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fos.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) imgViewTakePhoto.getDrawable();
+            Bitmap bitmap = bitmapDrawable.getBitmap();
+
+            OutputStream outputStream = contentResolver.openOutputStream(Objects.requireNonNull(uri));
+            bitmap.compress(Bitmap.CompressFormat.JPEG,100,outputStream);
+            Objects.requireNonNull(outputStream);
+
+            Toast.makeText(this, "Luu thanh cong", Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
-        return directory.getAbsolutePath();
+
+
+
     }
+
 }
+
+
+
+
